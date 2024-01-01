18d8aef05a8154e1c0d63fa4ef35921143c5d96f
KabirUlster <olawore-b@ulster.ac.uk>
2024-01-01 12:52:30 +0000

    project updated

--- production/model.py
+++ production/model.py
@@ -1,18 +1,17 @@
 # %%
-# import seaborn as sns
 import mlflow
 import argparse
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn import  preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
-# from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 
 # %%
 parser = argparse.ArgumentParser()
@@ -26,22 +25,16 @@ head = df_train_data.head()
 print('data head: ', head)
 
 # %%
-tail = df_train_data.tail()
-print('data tail: ',tail)
+df_train_data.tail()
 
 # %%
-data_shape = df_train_data.shape
-print("data shape: ", data_shape)
+df_train_data.shape
 
 # %%
-null_column = df_train_data.isnull().sum()
-
-print('check null columns: ', null_column)
+df_train_data.isnull().sum()
 
 # %%
-actvities = df_train_data['Activity'].unique()
-
-print('HAR actvities: ', actvities)
+df_train_data['Activity'].unique()
 
 # %%
 activity_counts = df_train_data['Activity'].value_counts()
@@ -50,13 +43,24 @@ plt.figure(figsize=(6,6))
 plt.pie(activity_counts, labels=activity_counts.index, autopct='%1.1f%%', startangle=90)
 plt.title('Distribution of Activities')
 plt.show()
+# plt.figure(figsize=(6,3))
+
+# axis = sns.countplot(x="Activity", data=df_train)
+# plt.xticks(x=df_train['Activity'], rotation='vertical')
+# plt.show()
 
 # %%
 df_train_data['subject'].unique()
 
 # %%
+# X = pd.DataFrame(df_train.drop(['Activity', 'subject'], axis=1))
+# y = df_train.Activity.values.astype(object)
+
 X = df_train_data.drop(['Activity', 'subject'], axis=1)
-y = df_train_data['Activity'].values.astype(object)
+# y = df_train['Activity'].values.astype(object)
+y = df_train_data['Activity'].values
+
+# print('length of X',len(X[0]))
 
 print(np.unique(df_train_data.Activity.values, return_counts=True))
 
@@ -64,30 +68,43 @@ print("X shape: ", X.shape)
 print("y shape: ", y.shape)
 
 # %%
+X.shape, y.shape
+
+# %%
 X.head()
 
 # %%
-y[1000]
+y[5]
 
 # %%
 X.info()
 
 # %%
-num_of_cols = X.select_dtypes(include='number').columns
-print("Number of numeric features: ", num_of_cols.size) 
+num_cols = X._get_numeric_data().columns
+print("Number of numeric features:", num_cols.size) 
 
 # %%
-label_encoder = preprocessing.LabelEncoder()
+# encoder = preprocessing.LabelEncoder()
 
 # %%
-label_encoder.fit(y)
-y = label_encoder.transform(y)
-y.shape, label_encoder.classes_
+# encoder.fit(y)
+# y = encoder.transform(y)
+y.shape
 
 # %%
-standard_scaler = StandardScaler()
+y[5]
 
-X = standard_scaler.fit_transform(X)
+# %%
+# encoder.classes_
+
+# %%
+# encoder.classes_[5]
+
+# %%
+scaler = StandardScaler()
+
+# %%
+X = scaler.fit_transform(X)
 X[5]
 
 # %%
@@ -95,53 +112,38 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 
 # %%
-X_train.shape, X_test.shape, y_train.shape, y_test.shape
+X_train.shape, X_test.shape, y_train.shape
 
 # %%
-def eval_model(true, predicted):
+def evaluate_model(true, predicted):
     accuracy = accuracy_score(true, predicted)
     precision = precision_score(true, predicted, average='weighted', zero_division=1)
     recall = recall_score(true, predicted, average='weighted', zero_division=1)
     f1 = f1_score(true, predicted, average='weighted', zero_division=1)
-    conf_matrix = confusion_matrix(true, predicted)
     
-    return accuracy, precision, recall, f1, conf_matrix
+    return accuracy, precision, recall, f1
 
 
 # %%
 models = {
-    "SVM": SVC(kernel='rbf',C=100.0),
     # "Logistic Regression": LogisticRegression(random_state=5, max_iter=2000),
-    # "SVM": SVC(),
+    "SVM": SVC(kernel='rbf',C=100.0),
 }
 
-list_of_model = []
-accuracy_list =[]
+model_list = []
+r2_list =[]
 
 # %%
 for i in range(len(list(models))):
     model = list(models.values())[i]
-    
-    # Train model
-    model.fit(X_train, y_train) 
+    model.fit(X_train, y_train) # Train model
 
     # Make predictions
-    # y_train_prediction = model.predict(X_train)
-    y_test_prediction = model.predict(X_test)
+    y_test_pred = model.predict(X_test)
 
-    # model_train_accuracy , model_train_precision, model_train_recall, model_train_f1, model_train_cm = eval_model(y_train, y_train_prediction)
+    model_test_accuracy , model_test_precision, model_test_recall, model_test_f1 = evaluate_model(y_test, y_test_pred)
 
-    model_test_accuracy , model_test_precision, model_test_recall, model_test_f1, model_test_cm = eval_model(y_test, y_test_prediction)
-
-    print(list(models.keys())[i])
-    list_of_model.append(list(models.keys())[i])
-    
-    # print('Model performance for Training set')
-    # print("- Accuracy: {:.4f}".format(model_train_accuracy))
-    # print("- Precision: {:.4f}".format(model_train_precision))
-    # print("- Recall: {:.4f}".format(model_train_recall))
-    # print("- f1: {:.4f}".format(model_train_f1))
- 
+    model_list.append(list(models.keys())[i])
 
     print('----------------------------------')
     
@@ -150,14 +152,15 @@ for i in range(len(list(models))):
     print("- Precision: {:.4f}".format(model_test_precision))
     print("- Recall: {:.4f}".format(model_test_recall))
     print("- f1: {:.4f}".format(model_test_f1))
-    
-    accuracy_list.append(model_test_accuracy)
+    # print("conf matrix: {}".format(model_test_cm))
+    r2_list.append(model_test_accuracy)
     
     print('='*35)
     print('\n')
 
 
 # %%
-result_df = pd.DataFrame(list(zip(list_of_model, accuracy_list)), columns=['Algorithm_Type', 'Accuracy_Score']).sort_values(by=["Accuracy_Score"],ascending=False)
+result_df = pd.DataFrame(list(zip(model_list, r2_list)), columns=['Algorithm_Type', 'Accuracy_Score']).sort_values(by=["Accuracy_Score"],ascending=False)
 print(result_df)
-# %%
+
+
